8573735d7f926aed27418772aa1fd6c85d6d0e4d
Chris Wilson <chris+github@qwirx.com>
2012-01-10 17:35:44 +0000

    Test the menu tag and global context objects.

--- django/intranet/binder/tests.py
+++ django/intranet/binder/tests.py
@@ -8,10 +8,27 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 import settings
+import documents.urls
+import binder.templatetags.menu as menu_tag
 
 class BinderTest(TestCase):
     def test_front_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['global'],
-            settings.GLOBAL_CONTEXT['global'])
+        
+        g = response.context['global']
+        self.assertEqual("/", g['path'])
+        self.assertEqual(settings.APP_TITLE, g['app_title'])
+        
+        main_menu = g['main_menu']
+        self.assertEqual("Home", main_menu[0].title)
+        self.assertEqual("front_page", main_menu[0].url_name)
+        self.assertEqual("Documents", main_menu[1].title)
+        self.assertEqual(documents.urls.urlpatterns[0].name,
+            main_menu[1].url_name)
+        
+    def test_menu_tag(self):
+        response = self.client.get('/')
+        self.assertEqual('<li class="selected"><a href="/">Home</a></li>',
+            menu_tag.menu_item(response.context, 'front_page', 'Home'))
+            
