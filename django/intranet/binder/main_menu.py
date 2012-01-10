2fd36181eb4dc3a52b834e706635bbd9ae3f31a3
Chris Wilson <chris+github@qwirx.com>
2012-01-10 17:16:24 +0000

    Use template tags to generate menu items with proper highlighting.

--- django/intranet/binder/main_menu.py
+++ django/intranet/binder/main_menu.py
@@ -2,16 +2,16 @@ from collections import namedtuple
 
 from django.core.urlresolvers import reverse, NoReverseMatch
 
-Generator = namedtuple('Generator', ('link_name', 'title'))
+Generator = namedtuple('Generator', ('url_name', 'title'))
 Item = namedtuple('Item', ('href', 'title'))
 
 class Menu:
     generators = []
     
     def __getitem__(self, key):
-        g = self.generators[key]
-        return Item(reverse(g.link_name), g.title)
-        # return self.generators[key]
+        # g = self.generators[key]
+        # return Item(reverse(g.link_name), g.title)
+        return self.generators[key]
     
     def append(self, title, url_name):
         self.generators.append(Generator(url_name, title))
