68800fd25caa40c3145c47b1980dd2c96680260c
Chris Wilson <chris+github@qwirx.com>
2012-01-10 17:35:33 +0000

    Enable Django admin interface.

--- django/intranet/settings.py
+++ django/intranet/settings.py
@@ -123,9 +123,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'binder',
-    'documents'
+    'documents',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
