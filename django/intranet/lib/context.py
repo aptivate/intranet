186003898b2e1a0a4f4043685c50505bc27cf068
Chris Wilson <chris+github@qwirx.com>
2012-01-11 14:52:39 +0000

    Set the root_path for admin templates.

--- django/intranet/lib/context.py
+++ django/intranet/lib/context.py
@@ -4,8 +4,11 @@ import settings
 import binder.main_menu
 
 def intranet_global(request):
-    return {'global': {
-        'app_title': settings.APP_TITLE,
-        'path': request.path,
-        'main_menu': binder.main_menu.MAIN_MENU,
-        }}
+    return {
+        'global': {
+            'app_title': settings.APP_TITLE,
+            'path': request.path,
+            'main_menu': binder.main_menu.MAIN_MENU,
+        },
+        'root_path': '/admin',
+    }
