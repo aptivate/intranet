acbd96d339a21de8e96d1a092013cf2eb06a25db
Chris Wilson <chris+github@qwirx.com>
2012-01-13 15:54:55 +0000

    Add document type and program models, update document fields.

--- django/intranet/documents/models.py
+++ django/intranet/documents/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # http://djangosnippets.org/snippets/1054/
 
+class DocumentType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    def __unicode__(self):
+        return self.name
+
+class Program(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    def __unicode__(self):
+        return self.name
+
 class Document(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    created = models.DateTimeField(auto_now_add = True)
-    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=255, unique=True)
+    document_type = models.ForeignKey(DocumentType)
+    programs = models.ManyToManyField(Program)
     file = models.FileField(upload_to='documents')
+    notes = models.TextField()
+    authors = models.ManyToManyField(User)
+    created = models.DateTimeField(auto_now_add = True)
     
     def __unicode__(self):
         return self.title
