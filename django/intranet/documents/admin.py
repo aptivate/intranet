79c4099e3fa06a2f4a3fa24e45c37f4496d3a902
Chris Wilson <chris+github@qwirx.com>
2012-01-13 15:54:05 +0000

    Register admin interface for document types and programs.

--- django/intranet/documents/admin.py
+++ django/intranet/documents/admin.py
@@ -4,6 +4,23 @@ from django.contrib import admin
 import models
 
 class DocumentAdmin(admin.ModelAdmin):
-    pass
+    def get_urls(self):
+        urlpatterns = super(DocumentAdmin, self).get_urls()
+        # print "patterns = %s" % urlpatterns
+        # print "name = %s" % urlpatterns[0].name
+        return urlpatterns
+    
+    """
+    def has_add_permission(self, request):
+        print "user = %s" % request.user
+        opts = self.opts
+        perm = opts.app_label + '.' + opts.get_add_permission()
+        print "perm = %s" % perm
+        print "has_perm = %s" % request.user.has_perm(perm)
+        return admin.ModelAdmin.has_add_permission(self, request)
+        """
 
 admin.site.register(models.Document, DocumentAdmin)
+
+admin.site.register(models.DocumentType, admin.ModelAdmin)
+admin.site.register(models.Program, admin.ModelAdmin)
