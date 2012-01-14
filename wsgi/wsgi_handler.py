84d3d6a1cc49e5728d7be1ea3712bbeeba123190
Chris Wilson <chris+github@qwirx.com>
2012-01-14 18:37:05 +0000

    Revert SVN patch 6440 as it breaks deployment:
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] mod_wsgi (pid=9654): Target WSGI script '/var/django/intranet/dev/wsgi/wsgi_handler.py' cannot be loaded as Python module.
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] mod_wsgi (pid=9654): Exception occurred processing WSGI script '/var/django/intranet/dev/wsgi/wsgi_handler.py'.
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] Traceback (most recent call last):
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34]   File "/var/django/intranet/dev/wsgi/wsgi_handler.py", line 32, in <module>
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34]     project_name, 'settings'))
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] ImportError: No module named /var/django/intranet/dev/django/intranet/settings
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] mod_wsgi (pid=9654): Target WSGI script '/var/django/intranet/dev/wsgi/wsgi_handler.py' cannot be loaded as Python module.
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] mod_wsgi (pid=9654): Exception occurred processing WSGI script '/var/django/intranet/dev/wsgi/wsgi_handler.py'.
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] Traceback (most recent call last):
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34]   File "/var/django/intranet/dev/wsgi/wsgi_handler.py", line 32, in <module>
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34]     project_name, 'settings'))
    [Sat Jan 14 18:15:20 2012] [error] [client 10.0.156.34] ImportError: No module named /var/django/intranet/dev/django/intranet/settings

--- wsgi/wsgi_handler.py
+++ wsgi/wsgi_handler.py
@@ -16,41 +16,16 @@ site.addsitedir(os.path.join(project_dir, 'django', project_name, '.ve',
 sys.path.append(os.path.join(project_dir, 'django'))
 sys.path.append(os.path.join(project_dir, 'django', project_name))
 
+
+#print >> sys.stderr, sys.path
+
+os.environ['DJANGO_SETTINGS_MODULE'] = project_name + '.settings'
+
 # this basically does:
 # os.environ['PROJECT_NAME_HOME'] = '/var/django/project_name/dev/'
 os.environ[project_name.upper() + '_HOME'] = os.path.join('/var/django', project_name, 'dev')
 
-# this does the same setup as "./manage.py runserver", ensuring we get the
-# same behaviour on apache as when developing.
-# See http://blog.dscpl.com.au/2010/03/improved-wsgi-script-for-use-with.html
-# for the rationale.
-
-# import project_name.settings as settings
-# we want the above, but to use a string we do
-import imp
-fp, pathname, desc = imp.find_module(os.path.join(project_dir, 'django',
-                                   project_name, 'settings'))
-try:
-    settings = imp.load_module(project_name+'.settings', fp, pathname, desc)
-finally:
-    if fp:
-        fp.close()
-
-import django.core.management
-django.core.management.setup_environ(settings)
-utility = django.core.management.ManagementUtility()
-command = utility.fetch_command('runserver')
-command.validate()
-
-import django.conf
-import django.utils
-django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
-
-# Now we do the normal django set up
 import django.core.handlers.wsgi
 
 application = django.core.handlers.wsgi.WSGIHandler()
 
-# Dozer is something that can help debug memory leaks
-#from dozer import Dozer
-#application = Dozer(application)
