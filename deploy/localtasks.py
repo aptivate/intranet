d3af77706b560f74e414b76431c02f3d9f52b4ba
Chris Wilson <chris+github@qwirx.com>
2012-01-16 11:37:44 +0000

    Attempted fix for getting the MEDIA_ROOT out of settings.py.

--- deploy/localtasks.py
+++ deploy/localtasks.py
@@ -4,11 +4,14 @@ from tasklib import env
 def post_deploy(environment=None):
     sys.path.append(env['django_dir'])
     sys.path.append(os.path.join(env['ve_dir'], 'lib', 'python2.6', 'site-packages'))
+
     import settings as django_settings
     
-    if 'MEDIA_ROOT' in django_settings:
-        if django_settings.MEDIA_ROOT is not None and \
-            django_settings.MEDIA_ROOT != '':
-            mkdir(django_settings.MEDIA_ROOT)
-            os.chmod(django_settings.MEDIA_ROOT, stat.S_IRWXU |
-                stat.S_IRWXG | stat.S_IRWXO)
+    try:
+        from settings import MEDIA_ROOT
+        if MEDIA_ROOT is not None and MEDIA_ROOT != '':
+            mkdir(MEDIA_ROOT)
+            os.chmod(MEDIA_ROOT, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    except ImportError, e:
+        # no MEDIA_ROOT? not a fatal error
+        print "No MEDIA_ROOT found: %s" % e
