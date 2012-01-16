2fcf94c99929bde0a273a29de762b5b0247a7900
Chris Wilson <chris+github@qwirx.com>
2012-01-16 11:24:25 +0000

    Try including virtualenv lib directory so that django import works in settings.py.

--- deploy/localtasks.py
+++ deploy/localtasks.py
@@ -3,7 +3,9 @@ from tasklib import env
 
 def post_deploy(environment=None):
     sys.path.append(env['django_dir'])
+    sys.path.append(os.path.join(env['ve_dir'], 'lib', 'python2.6'))
     import settings as django_settings
+    
     if 'MEDIA_ROOT' in django_settings:
         if django_settings.MEDIA_ROOT is not None and \
             django_settings.MEDIA_ROOT != '':
