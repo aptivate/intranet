7b23ef4909712e133112ceeeec6018ac920ef231
Chris Wilson <chris+github@qwirx.com>
2012-01-16 15:00:13 +0000

    Disable the "add" button on relations <https://code.djangoproject.com/ticket/17547>

--- django/intranet/documents/admin.py
+++ django/intranet/documents/admin.py
@@ -3,7 +3,35 @@
 from django.contrib import admin
 import models
 
+from django.contrib.admin.widgets import RelatedFieldWidgetWrapper
+
+class RelatedFieldWithoutAddLink(RelatedFieldWidgetWrapper):
+    def __init__(self, widget, rel, admin_site, can_add_related=None):
+        RelatedFieldWidgetWrapper.__init__(self, widget, rel, admin_site,
+            can_add_related=False)
+
+from django.forms.models import ModelForm
+
+class DocumentAdminForm(ModelForm):
+    class Meta:
+        model = models.Document
+        # widgets = {'document_type': RelatedFieldWithoutAddLink,}
+
 class DocumentAdmin(admin.ModelAdmin):
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        old_formfield = admin.ModelAdmin.formfield_for_dbfield(self, db_field,
+            **kwargs)
+        if (hasattr(old_formfield, 'widget') and
+            isinstance(old_formfield.widget, RelatedFieldWidgetWrapper)):
+            old_formfield.widget.can_add_related = False
+        return old_formfield
+    
+    """
+    def get_form(self, request, obj=None, **kwargs):
+        new_args = dict(form=DocumentAdminForm, **kwargs)
+        return admin.ModelAdmin.get_form(self, request, obj=obj, **new_args)
+    """
+    
     def get_urls(self):
         urlpatterns = super(DocumentAdmin, self).get_urls()
         # print "patterns = %s" % urlpatterns
