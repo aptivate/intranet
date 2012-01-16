1614979cbce691e7917888940898bc4d64a82bf1
Chris Wilson <chris+github@qwirx.com>
2012-01-16 17:09:03 +0000

    Tweak documents page, error message formatting.

--- django/intranet/documents/admin.py
+++ django/intranet/documents/admin.py
@@ -17,7 +17,62 @@ class DocumentAdminForm(ModelForm):
         model = models.Document
         # widgets = {'document_type': RelatedFieldWithoutAddLink,}
 
+from django.db import models as django_fields
+import django.contrib.admin
+from django.forms.util import flatatt as attributes_to_str
+from django.utils.safestring import mark_safe
+
+class URLFieldWidgetWithLink(admin.widgets.AdminURLFieldWidget):
+    def render(self, name, value, attrs=None):
+        html = admin.widgets.AdminURLFieldWidget.render(self, name, value,
+            attrs=attrs)
+
+        if value is not None:
+            final_attrs = dict(href=value, target='_blank')
+            html += " <a %s>(open)</a>" % attributes_to_str(final_attrs)
+        
+        return mark_safe(html)
+
+from django.forms.widgets import ClearableFileInput, CheckboxInput
+from django.template.defaultfilters import filesizeformat
+from django.utils.encoding import force_unicode
+from django.utils.html import escape, conditional_escape
+
+class AdminFileWidgetWithSize(admin.widgets.AdminFileWidget):
+    template_with_initial = u'%(initial_text)s: %(initial)s (%(size)s) %(clear_template)s<br />%(input_text)s: %(input)s'
+
+    def render(self, name, value, attrs=None):
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': self.clear_checkbox_label,
+        }
+        template = u'%(input)s'
+        substitutions['input'] = super(ClearableFileInput, self).render(name, value, attrs)
+
+        if value and hasattr(value, "url"):
+            template = self.template_with_initial
+            substitutions['size'] = filesizeformat(value.size)
+            substitutions['initial'] = (u'<a href="%s">%s</a>'
+                                        % (escape(value.url),
+                                           escape(force_unicode(value))))
+            if not self.is_required:
+                checkbox_name = self.clear_checkbox_name(name)
+                checkbox_id = self.clear_checkbox_id(checkbox_name)
+                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
+                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
+                substitutions['clear'] = CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+        
+        return mark_safe(template % substitutions)
+
 class DocumentAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        django_fields.URLField: {'widget': URLFieldWidgetWithLink},
+        django_fields.FileField: {'widget': AdminFileWidgetWithSize},
+    }
+    
     def formfield_for_dbfield(self, db_field, **kwargs):
         old_formfield = admin.ModelAdmin.formfield_for_dbfield(self, db_field,
             **kwargs)
