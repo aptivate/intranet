0334e7a963509b325193a8a502a1db69847faffa
Chris Wilson <chris+github@qwirx.com>
2012-01-16 15:14:49 +0000

    Add a Hyperlink field to Document model.

--- django/intranet/documents/models.py
+++ django/intranet/documents/models.py
@@ -17,10 +17,20 @@ class Document(models.Model):
     title = models.CharField(max_length=255, unique=True)
     document_type = models.ForeignKey(DocumentType)
     programs = models.ManyToManyField(Program)
-    file = models.FileField(upload_to='documents')
+    file = models.FileField(upload_to='documents', blank=True)
     notes = models.TextField()
     authors = models.ManyToManyField(User)
     created = models.DateTimeField(auto_now_add = True)
+    hyperlink = models.URLField(blank=True)
     
     def __unicode__(self):
         return self.title
+
+    def clean(self):
+        models.Model.clean(self)
+        
+        from django.core.exceptions import ValidationError
+        
+        if not self.file and not self.hyperlink:
+            raise ValidationError('You must either attach a file ' +
+                'or provide a hyperlink')
