56ed7e12c6defdeef4751eeb6cc0b84035cc623b
Chris Wilson <chris+github@qwirx.com>
2012-01-19 15:59:30 +0000

    Add PDF indexing support using pdfminer.

--- django/intranet/documents/search_indexes.py
+++ django/intranet/documents/search_indexes.py
@@ -8,8 +8,30 @@ from django.core.files.uploadedfile import TemporaryUploadedFile, \
 from haystack.indexes import *
 from haystack import site
 from magic import Magic
+from StringIO import StringIO
 
 from models import Document
+
+from pdfminer.pdfparser import PDFDocument, PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter, process_pdf
+from pdfminer.pdfdevice import PDFDevice, TagExtractor
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.cmapdb import CMapDB
+from pdfminer.layout import LAParams
+from pdfminer.layout import LTContainer, LTText, LTTextBox
+
+class TextConverterWithoutPageBreaks(TextConverter):
+    def receive_layout(self, ltpage):
+        def render(item):
+            if isinstance(item, LTContainer):
+                for child in item:
+                    render(child)
+            elif isinstance(item, LTText):
+                self.write_text(item.get_text())
+            if isinstance(item, LTTextBox):
+                self.write_text('\n')
+        render(ltpage)
+        return
 
 class DocumentIndex(RealTimeSearchIndex):
     text = CharField(model_attr='file', document=True)
@@ -68,52 +90,62 @@ class DocumentIndex(RealTimeSearchIndex):
         print "upload_to = %s" % document.file.field.upload_to
         """
 
-        if document.file is not None:
-            try:
-                document.file.open()
-                f = document.file.file
-                magic = Magic(mime=True)
-                mime = magic.from_buffer(f.read())
-                f.seek(0) # reset to beginning
-            
-                if mime == 'application/zip' or mime == 'application/x-zip':
-                    # is it a DOCX file?
-                    document = docx.opendocx(document.file)
-                    paratextlist = docx.getdocumenttext(document)
-                    return "\n\n".join(paratextlist)
-                elif mime == 'application/msword':
-                    if isinstance(f, InMemoryUploadedFile):
-                        tmp = TemporaryUploadedFile(name=f.name,
-                            content_type=f.content_type, size=f.size,
-                            charset=f.charset)
-                        tmp.write(f.read())
-                        f.close()
-                        f = tmp
-                        
-                    if isinstance(f, TemporaryUploadedFile):
-                        path = f.temporary_file_path()
-                    else:
-                        path = f.name
-                    
-                    try:
-                        process = self.safe_popen('antiword', path)
-                    except Exception as e:
-                        raise Exception('Failed to convert Word document: %s' %
-                            e);
-                        
-                    (out, err) = process.communicate()
-                    if err != '':
-                        err = err.replace(path, document.file.name)
-                        raise Exception('Failed to convert Word document: %s' %
-                            err);
-                    return out
-                else:
-                    raise Exception("Don't know how to index %s documents" %
-                        mime)
-                    # data = f.read(1<<26) # 64 MB
-                    # return data
-            finally:
-                if f is not None:
+        if document.file is None:
+            return
+        
+        try:
+            document.file.open()
+            f = document.file.file
+            magic = Magic(mime=True)
+            mime = magic.from_buffer(f.read())
+            f.seek(0) # reset to beginning
+        
+            if mime == 'application/zip' or mime == 'application/x-zip':
+                # is it a DOCX file?
+                document = docx.opendocx(document.file)
+                paratextlist = docx.getdocumenttext(document)
+                return "\n\n".join(paratextlist)
+            elif mime == 'application/msword':
+                if isinstance(f, InMemoryUploadedFile):
+                    tmp = TemporaryUploadedFile(name=f.name,
+                        content_type=f.content_type, size=f.size,
+                        charset=f.charset)
+                    tmp.write(f.read())
                     f.close()
+                    f = tmp
+                    
+                if isinstance(f, TemporaryUploadedFile):
+                    path = f.temporary_file_path()
+                else:
+                    path = f.name
+                
+                try:
+                    process = self.safe_popen('antiword', path)
+                except Exception as e:
+                    raise Exception('Failed to convert Word document: %s' %
+                        e);
+                    
+                (out, err) = process.communicate()
+                if err != '':
+                    err = err.replace(path, document.file.name)
+                    raise Exception('Failed to convert Word document: %s' %
+                        err);
+                return out
+            elif mime == 'application/pdf':
+                rsrcmgr = PDFResourceManager(caching=True)
+                outfp = StringIO()
+                device = TextConverterWithoutPageBreaks(rsrcmgr, outfp,
+                    codec='utf-8', laparams=LAParams())
+                process_pdf(rsrcmgr, device, f, caching=True,
+                    check_extractable=False)
+                return outfp.getvalue()
+            else:
+                raise Exception("Don't know how to index %s documents" %
+                    mime)
+                # data = f.read(1<<26) # 64 MB
+                # return data
+        finally:
+            if f is not None:
+                f.close()
 
 site.register(Document, DocumentIndex)
