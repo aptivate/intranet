4fa60b1930683e1c6390a422e0f3afb2100b054c
Chris Wilson <chris+github@qwirx.com>
2012-01-24 16:32:35 +0000

    Restrict Guest to documents in their Program if they have one.

--- django/intranet/documents/admin.py
+++ django/intranet/documents/admin.py
@@ -153,117 +153,19 @@ class DocumentAdmin(django.contrib.admin.ModelAdmin):
     
     def get_changelist(self, request, **kwargs):
         return ChangeListWithLinksToReadOnlyView
-
-    """
-    def readonly_view(self, request, object_id, extra_context=None):
-        "The 'change' admin view for this model."
-        model = self.model
-        opts = model._meta
-
-        from django.contrib.admin.util import unquote, flatten_fieldsets, \
-            get_deleted_objects, model_format_dict
-
-        obj = self.get_object(request, unquote(object_id))
-
-        from django.core.exceptions import PermissionDenied, ValidationError
-
-        if not self.has_change_permission(request, obj):
-            raise PermissionDenied
-
-        from django.http import Http404, HttpResponse, HttpResponseRedirect
-        from django.utils.encoding import force_unicode
-        from django.utils.html import escape, escapejs
-
-        if obj is None:
-            raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
-
-        if request.method == 'POST' and "_saveasnew" in request.POST:
-            return self.add_view(request, form_url='../add/')
-
-        ModelForm = self.get_form(request, obj)
-        formsets = []
-        if request.method == 'POST':
-            form = ModelForm(request.POST, request.FILES, instance=obj)
-            if form.is_valid():
-                form_validated = True
-                new_object = self.save_form(request, form, change=True)
-            else:
-                form_validated = False
-                new_object = obj
-            prefixes = {}
-            for FormSet, inline in zip(self.get_formsets(request, new_object),
-                                       self.inline_instances):
-                prefix = FormSet.get_default_prefix()
-                prefixes[prefix] = prefixes.get(prefix, 0) + 1
-                if prefixes[prefix] != 1:
-                    prefix = "%s-%s" % (prefix, prefixes[prefix])
-                formset = FormSet(request.POST, request.FILES,
-                                  instance=new_object, prefix=prefix,
-                                  queryset=inline.queryset(request))
-
-                formsets.append(formset)
-
-            if all_valid(formsets) and form_validated:
-                self.save_model(request, new_object, form, change=True)
-                form.save_m2m()
-                for formset in formsets:
-                    self.save_formset(request, form, formset, change=True)
-
-                change_message = self.construct_change_message(request, form, formsets)
-                self.log_change(request, new_object, change_message)
-                return self.response_change(request, new_object)
-
-        else:
-            form = ModelForm(instance=obj)
-            prefixes = {}
-            for FormSet, inline in zip(self.get_formsets(request, obj), self.inline_instances):
-                prefix = FormSet.get_default_prefix()
-                prefixes[prefix] = prefixes.get(prefix, 0) + 1
-                if prefixes[prefix] != 1:
-                    prefix = "%s-%s" % (prefix, prefixes[prefix])
-                formset = FormSet(instance=obj, prefix=prefix,
-                                  queryset=inline.queryset(request))
-                formsets.append(formset)
-
-        adminForm = helpers.AdminForm(form, self.get_fieldsets(request, obj),
-            self.prepopulated_fields, self.get_readonly_fields(request, obj),
-            model_admin=self)
-        media = self.media + adminForm.media
-
-        inline_admin_formsets = []
-        for inline, formset in zip(self.inline_instances, formsets):
-            fieldsets = list(inline.get_fieldsets(request, obj))
-            readonly = list(inline.get_readonly_fields(request, obj))
-            inline_admin_formset = helpers.InlineAdminFormSet(inline, formset,
-                fieldsets, readonly, model_admin=self)
-            inline_admin_formsets.append(inline_admin_formset)
-            media = media + inline_admin_formset.media
-
-        context = {
-            'title': _('Change %s') % force_unicode(opts.verbose_name),
-            'adminform': adminForm,
-            'object_id': object_id,
-            'original': obj,
-            'is_popup': "_popup" in request.REQUEST,
-            'media': mark_safe(media),
-            'inline_admin_formsets': inline_admin_formsets,
-            'errors': helpers.AdminErrorList(form, formsets),
-            'root_path': self.admin_site.root_path,
-            'app_label': opts.app_label,
-        }
-        context.update(extra_context or {})
-        return self.render_change_form(request, context, change=True, obj=obj)
-    """
     
-    """
-    def has_add_permission(self, request):
-        print "user = %s" % request.user
-        opts = self.opts
-        perm = opts.app_label + '.' + opts.get_add_permission()
-        print "perm = %s" % perm
-        print "has_perm = %s" % request.user.has_perm(perm)
-        return admin.ModelAdmin.has_add_permission(self, request)
-        """
+    def queryset(self, request):
+        if request.user.groups.filter(name='Guest'):
+            limit_to_program = request.user.program
+        else:
+            limit_to_program = None
+
+        qs = super(self.__class__, self).queryset(request)
+        
+        if limit_to_program:
+            return qs.filter(programs=limit_to_program)
+        else:
+            return qs
 
 django.contrib.admin.site.register(models.Document, DocumentAdmin)
 
