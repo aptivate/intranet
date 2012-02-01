5c420de957aba36e5e6408a81f494d8768a623f7
root <root@fen-vz-atamis.fen.aptivate.org>
2012-02-01 12:04:51 +0000

    Improve argument handling logic and readability, allow passing --vmarg args.

--- tika/setup.py
+++ tika/setup.py
@@ -16,52 +16,53 @@ options = {
 import sys
 import os.path
 
-args = []
+jcc_args = []
 
 for dir in sys.path:
     probe_dir = os.path.join(dir, 'jcc')
     if os.path.exists(probe_dir):
         print "Found jcc library at: %s" % probe_dir
-        args = [os.path.join(probe_dir, 'nonexistent-argv-0')]
+        jcc_args = [os.path.join(probe_dir, 'nonexistent-argv-0')]
         break
 
-if not args:
+if not jcc_args:
     raise Exception("tika library not found in sys.path: %s" % sys.path)
 
 for k, v in options.iteritems():
     if k == 'classes':
-        args.extend(v)
+        jcc_args.extend(v)
     elif hasattr(v, '__iter__'):
         for value in v:
-            args.append('--%s' % k)
-            args.append(value)
+            jcc_args.append('--%s' % k)
+            jcc_args.append(value)
     else:
-        args.append('--%s' % k)
-        args.append(v)
+        jcc_args.append('--%s' % k)
+        jcc_args.append(v)
 
-forwarded_args = []
+setup_args = []
 
-skip_next_arg = False
-forward_next_arg = False
 egg_info_mode = False
 
-for arg in sys.argv[1:]:
-    if skip_next_arg:
-        skip_next_arg = False
-    elif forward_next_arg:
-        forward_next_arg = False
-        forwarded_args.append(arg)
-    elif arg == 'install':
-        args.append('--install')
+i = 1
+
+while i < len(sys.argv):
+    arg = sys.argv[i]
+
+    if arg == 'install':
+        jcc_args.append('--install')
     elif arg == 'build':
-        args.append('--build')
+        jcc_args.append('--build')
     elif arg == '-c':
         # forwarded_args.append(arg)
         pass
     elif arg == 'egg_info':
-        args.append('--egg-info')
+        jcc_args.append('--egg-info')
+    elif arg == '--vmarg':
+        jcc_args.append(arg)
+        i += 1
+        jcc_args.append(sys.argv[i])
     else:
-        forwarded_args.append(arg)
+        setup_args.append(arg)
     """
     elif arg == '--single-version-externally-managed':
         forwarded_args.append(arg)
@@ -71,10 +72,12 @@ for arg in sys.argv[1:]:
     else:
         raise NotImplementedError("Unknown argument: %s" % arg)
     """
+
+    i += 1
     
-for extra_arg in forwarded_args:
-    args.append('--extra-setup-arg')
-    args.append(extra_arg)
+for extra_arg in setup_args:
+    jcc_args.append('--extra-setup-arg')
+    jcc_args.append(extra_arg)
 
 # monkey patch to send extra args to distutils
 # import setuptools
@@ -95,6 +98,6 @@ def new_compile(**kwargs):
     compile(**kwargs, new_setup)
 """
 
-print "args = %s" % args
+print "jcc_args = %s" % jcc_args
 
-cpp.jcc(args)
+cpp.jcc(jcc_args)
