85a96cbe0a4c3492075f69e2668fb922a8c462bb
Chris Wilson <chris+github@qwirx.com>
2012-02-02 12:09:58 +0000

    Fix Documents module tests to make TDD possible again.

--- django/intranet/documents/tests.py
+++ django/intranet/documents/tests.py
@@ -10,7 +10,7 @@ from lxml import etree
 
 from django.test import TestCase
 from django.contrib import admin 
-from django.contrib.auth.models import User, Group, Permission
+from binder.models import IntranetUser
 from django.contrib.auth import login
 from django.conf import settings as django_settings
 from django.core.urlresolvers import reverse
@@ -20,12 +20,13 @@ from django.test.client import Client, RequestFactory, encode_multipart, \
 from django.utils.functional import curry
 
 from documents.admin import DocumentAdmin
-from documents.models import Document, Program, DocumentType
+from documents.models import Document, DocumentType
+from binder.models import Program
 
 class SuperClient(Client):
-    def get(self, *args):
+    def get(self, *args, **extra):
         response = Client.get(self, *args)
-        return self.capture_results('get', response, *args)
+        return self.capture_results('get', response, *args, **extra)
 
     def post(self, path, data={}, content_type=MULTIPART_CONTENT,
              **extra):
@@ -44,29 +45,69 @@ class SuperClient(Client):
         # print "session cookie = %s" % (
         # self.cookies[django_settings.SESSION_COOKIE_NAME])
         response = Client.post(self, path, data, content_type, **extra)
+        
+        if response is None:
+            raise Exception("POST method responded with None!")
+        
         return self.capture_results('post', response, path, data,
             content_type, **extra)
     
-    def capture_results(self, method_name, response, *args):
+    def capture_results(self, method_name, response, *args, **kwargs):
         # print("%s.%s(%s)" % (self, method_name, args))
         self.last_method = method_name
         self.last_method_args = args
+        self.last_method_kwargs = kwargs
+        
+        if not response.content:
+            return response # without setting the parsed attribute
+        
         # http://stackoverflow.com/questions/5170252/whats-the-best-way-to-handle-nbsp-like-entities-in-xml-documents-with-lxml
         x = """<?xml version="1.0" encoding="utf-8"?>\n""" + response.content
         p = etree.XMLParser(remove_blank_text=True, resolve_entities=False)
-        r = etree.fromstring(x, p)
+        
+        try:
+            r = etree.fromstring(x, p)
+        except SyntaxError as e:
+            import re
+            match = re.match('Opening and ending tag mismatch: ' +
+                '(\w+) line (\d+) and (\w+), line (\d+), column (\d+)', str(e))
+            if match:
+                lineno = int(match.group(2))
+            else:
+                match = re.match('.*, line (\d+), column (\d+)', str(e))
+                if match:
+                    lineno = int(match.group(1))
+
+            if not match:            
+                lineno = e.lineno
+                
+            lines = x.splitlines(True)
+            if lineno is not None:
+                first_line = max(lineno - 5, 1)
+                last_line = min(lineno + 5, len(lines))
+                print x
+                print "Context (line %s):\n%s" % (lineno,
+                    "".join(lines[first_line:last_line]))
+            else:
+                print repr(e)
+            raise e  
+        
         setattr(response, 'parsed', r)
         return response
         
     def retry(self):
         """Try the same request again (e.g. after login)."""
-        return getattr(self, self.last_method)(*self.last_method_args)
+        # print "retry kwargs = %s" % self.last_method_kwargs 
+        return getattr(self, self.last_method)(*self.last_method_args,
+            **self.last_method_kwargs)
     
     def request(self, **request):
         # print "request = %s" % request
         return Client.request(self, **request)
 
 class DocumentsModuleTest(TestCase):
+    fixtures = ['test_permissions', 'test_users']
+    
     """
     def wrap(self, instance, method_name):
         old_method = getattr(instance, method_name)
@@ -86,52 +127,22 @@ class DocumentsModuleTest(TestCase):
     """
      
     def setUp(self):
-        from django.contrib.contenttypes.models import ContentType
-        document_ct = ContentType.objects.get(app_label='documents',
-            model='document')
-
-        add_doc = Permission.objects.get(content_type=document_ct,
-            codename="add_document")
-        user = Group.objects.get(name='User')
-        self.assertIn(add_doc, user.permissions.all(),
-            'add document permission for User is required for tests to run')
-        
-        self.john = User.objects.create_user('john', 'lennon@thebeatles.com',
-            'johnpassword')
-        self.john.is_staff = True
-        self.john.groups.add(user)
-        self.john.save()
+        self.john = IntranetUser.objects.get(username='john')
 
         # run a POST just to get a response with its embedded request...
         self.login()
         response = self.client.post(reverse('admin:documents_document_add'))
         # fails with PermissionDenied if our permissions are wrong
-         
-        # response.request.__dict__ = {}
-        # setattr(response.request, 'user', self.john)
-        # that we can pass to has_add_permission()
-        # docadmin = admin.site._registry[Document]
-        # self.assertTrue(docadmin.has_add_permission(response.request),
-        #    "Adding permission to test user didn't work as expected")
-        
-        """
-        self.wrap(self.client, 'get')
-        self.wrap(self.client, 'post')
-        """
         self.client = SuperClient()
-
-    """        
-    def retry(self):
-        "Try the same request again (e.g. after login)."
-        return getattr(self.client, self.last_method)(*self.last_method_args)
-    """
     
     def login(self):
         self.assertTrue(self.client.login(username=self.john.username,
             password='johnpassword'), "Login failed")
         self.assertIn(django_settings.SESSION_COOKIE_NAME, self.client.cookies) 
+        """
         print "session cookie = %s" % (
             self.client.cookies[django_settings.SESSION_COOKIE_NAME])
+        """
         
     def test_create_document_object(self):
         doc = Document(title="foo", document_type=DocumentType.objects.all()[0],
@@ -188,7 +199,7 @@ class DocumentsModuleTest(TestCase):
                 'file': f,
                 'notes': 'whee',
                 'authors': self.john.id,
-            })
+            }, follow=True)
         self.assertEqual("Please check your user name and password and try again.",
             self.extract_error_message(response),
             "POST without login did not fail as expected: %s" % response.content)
@@ -197,21 +208,38 @@ class DocumentsModuleTest(TestCase):
         response = self.client.retry()
         # print response.content
         # print "%s" % response.context
-        self.assertDictEqual({}, response.context['adminform'].form.errors)
-        for fieldset in response.context['adminform']:
-            for line in fieldset:
-                self.assertIsNone(line.errors)
-                for field in line:
-                    self.assertIsNone(field.errors)
-        self.assertIsNone(response.context['adminform'].form.non_field_errors)
-        self.assertIsNone(self.extract_error_message(response))
+        self.assertTrue(hasattr(response, 'context'), "Missing context " +
+            "in response: %s: %s" % (response, dir(response)))
+        self.assertIsNotNone(response.context, "Empty context in response: " +
+            "%s: %s" % (response, dir(response)))
+        
+        # If this succeeds, we get redirected to the changelist_view.
+        # If it fails, we get sent back to the edit page, with an error.
+        if 'adminform' in response.context: 
+            self.assertDictEqual({}, response.context['adminform'].form.errors)
+            for fieldset in response.context['adminform']:
+                for line in fieldset:
+                    self.assertIsNone(line.errors)
+                    for field in line:
+                        self.assertIsNone(field.errors)
+            self.assertIsNone(response.context['adminform'].form.non_field_errors)
+            self.assertIsNone(self.extract_error_message(response))
+
+        self.assertNotIn('adminform', response.context, "Unexpected " +
+            "admin form in response context: %s" % response)
+        self.assertIn('cl', response.context, "Missing changelist " +
+            "in response context: %s" % response)
 
         # did it save?
         doc = Document.objects.get()
         self.assertEqual('foo', doc.title)
-        self.assertEqual('bar', doc.description)
-        self.assertSequenceEqual([self.john.id], doc.authors.all())
-        self.assertEqual('boink.png', doc.file)
+        self.assertEqual(DocumentType.objects.all()[0], doc.document_type)
+        self.assertItemsEqual([Program.objects.all()[0]], doc.programs.all())
+        import re
+        self.assertRegexpMatches(doc.file.name, 'boink(_\d+)?.png',
+            "Wrong name on uploaded file")
+        self.assertEqual('whee', doc.notes)
+        self.assertItemsEqual([self.john], doc.authors.all())
     
     """
     def test_admin_submit_with_error_doesnt_lose_file_data(self):
